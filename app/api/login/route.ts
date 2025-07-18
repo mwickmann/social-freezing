1d2ad23938b1333d7ed0544fc8c7cccefba4fc8d
mwickmann <wickmann93@gmail.com>
2025-07-18 09:36:17 +0200

    added right bcrypt

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,7 +1,7 @@
 // app/api/login/route.ts
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 export async function POST(req: NextRequest) {
   const { email, password } = await req.json()
