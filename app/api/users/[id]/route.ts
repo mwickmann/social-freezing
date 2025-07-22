8535b98f69e253d2bd1e864b1a51afcdf11d2d34
mwickmann <wickmann93@gmail.com>
2025-07-22 15:44:49 +0200

    added name to ui

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -13,6 +13,7 @@ export async function GET(req: NextRequest) {
       select: {
         id: true,
         email: true,
+        name: true,
         createdAt: true,
         eggCount: true,
       },
