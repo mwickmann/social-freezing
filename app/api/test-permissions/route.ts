b9530f403c3df4084ca0806f9686022c0446b416
mwickmann <wickmann93@gmail.com>
2025-07-22 11:03:02 +0200

    erorr type changed

--- app/api/test-permissions/route.ts
+++ app/api/test-permissions/route.ts
@@ -5,7 +5,11 @@ export async function GET() {
   try {
     const res = await prisma.$queryRaw`SELECT * FROM "User" LIMIT 1`;
     return NextResponse.json({ ok: true, data: res });
-  } catch (error: any) {
-    return NextResponse.json({ ok: false, error: error.message }, { status: 500 });
+  } catch (error: unknown) {
+  if (error instanceof Error) {
+    return NextResponse.json({ ok: false, error: error.message }, { status: 500 })
   }
+  return NextResponse.json({ ok: false, error: 'Ukjent feil' }, { status: 500 })
+}
+
 }
